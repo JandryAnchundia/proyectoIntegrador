d723c71e8ab984950e53c929b7c5aba17bc9a0de
Jandry_Anchundia <jandryorlando311098@gmail.com>
2022-06-03 00:15:59 -0500

    seccion de descripcion por empezar

---
+++ js/slider.js
@@ -0,0 +1,19 @@
+window.swiper = new Swiper({
+    el: '.slider__contenedor',
+    slideClass: 'slider__slide',
+    createElements: true,
+
+    autoplay: {
+        delay: 7000,
+    }, 
+    loop: true,
+    pagination: true,
+    navigation: true
+
+
+
+});
+
+
+
+
