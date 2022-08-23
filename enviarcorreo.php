b6b0a5991f7f4c35256b5dc0f2c4b4d5910d0a05
Jandry_Anchundia <jandryorlando311098@gmail.com>
2022-08-23 17:11:15 -0500

    Proyecto de tesis culminado al 100%

--- enviarcorreo.php
+++ enviarcorreo.php
@@ -1,17 +1,16 @@
 <?php
 
-$destino = "jandryorlando@gmail.com";
+$destino = "jandryorlando311098@gmail.com";
 //Esto es al correo que se enviarÃ¡ el mensaje
 
 $nombre = $_POST['nombre']; 
 $email = $_POST['email'];
 $telefono = $_POST['telefono'];
-$compañia = $_POST['compañia'];
 $asunto = $_POST['asunto'];
 $mensaje = $_POST['mensaje'];
 
-$header = "Enviado desde formulario de contacto en localhost";
-$mensajeCompleto = "\n Nombre: " . $nombre . "\n" . "Email: " . $email . "\n" . "Télefono :" . $telefono . "\n" . "Compañia :" . $compañia . "\n". "Mensaje :" . $mensaje;
+$header = "Enviado desde formulario de contacto del sitio: web Finca Tigrillo";
+$mensajeCompleto = "\n Nombre: " . $nombre . "\n" . "Email: " . $email . "\n" . "Télefono :" . $telefono . "\n". "Mensaje :" . $mensaje;
 mail($destino, $asunto, $mensajeCompleto, $header);
 header('Location: index.html');
 
